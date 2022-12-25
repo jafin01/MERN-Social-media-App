be56a645d5ae2d14d5e7d0f0e5b4278fbc9ee760
Jafin Jahfar <jafinjahfar@gmail.com>
2022-12-25 13:47:03 +0530

    added error middleware and multer middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import connectToDB from './config/db.js';
 import 'colors';
 
 const app = express();
 dotenv.config();
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 8000;
 
@@ -15,3 +27,5 @@ connectToDB(() => {
     console.log(`Listening to Port ${process.env.PORT}`);
   });
 });
+
+// Routes
